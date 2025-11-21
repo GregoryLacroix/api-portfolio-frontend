cd1a058ebdd0a774f362249ceb951872ffc0c341
GregoryLacroix <gregorylacroix78@gmail.com>
2025-11-21 20:51:59 +0100

    update mailto footer

--- src/components/frontend/Footer.jsx
+++ src/components/frontend/Footer.jsx
@@ -11,9 +11,20 @@ export default function () {
         </p>
         <div className={stylesFrontEnd.footer_reseaux}>
           <a
+            href="https://www.gregory-lacroix-pf.com/Grégory-LACROIX-DEV-11-2025.pdf"
+            className={stylesFrontEnd.footer__link}
+            download="CV_Gregory_Lacroix.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Curriculum vitae"
+          >
+            <span className={stylesFrontEnd.footer__link__cv}>CV</span>
+          </a>
+          <a
             href="https://www.linkedin.com/in/gregory-lacroix/"
             className={stylesFrontEnd.footer__link}
             target="_blank"
+            rel="noopener noreferrer"
           >
             <i className={"devicon-linkedin-plain " + stylesFrontEnd.icone__linkedin + " " + stylesFrontEnd.icone__footer}></i>
           </a>
@@ -21,6 +32,7 @@ export default function () {
             href="https://github.com/GregoryLacroix"
             className={stylesFrontEnd.footer__link}
             target="_blank"
+            rel="noopener noreferrer"
           >
             <i className={"devicon-github-original " + stylesFrontEnd.icone__github + " " + stylesFrontEnd.icone__footer}></i>
           </a>
