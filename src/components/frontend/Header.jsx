e96e53c146dd7b43ad93ef1f272c745a909b20f5
GregoryLacroix <gregorylacroix78@gmail.com>
2025-11-21 18:33:55 +0100

    update skills

--- src/components/frontend/Header.jsx
+++ src/components/frontend/Header.jsx
@@ -155,7 +155,7 @@ const scrollToPortfolio = (e) => {
           />
           <h1 className={stylesFrontEnd.header__title}>Grégory Lacroix</h1>
           <h2 className={stylesFrontEnd.header__slogan}>
-            Bonjour ! Je suis {text}
+            👋 Bonjour ! Je suis {text}
           </h2>
         </div>
       </section>
